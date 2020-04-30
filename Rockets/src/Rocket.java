25c50ed78ccd7fed1ea39d235c4485591bd39927
Cristian Toison <ctoison@edu.tecnocampus.cat>
2020-04-30 13:14:30 +0200

    rocket bro

--- Rockets/src/Rocket.java
+++ Rockets/src/Rocket.java
@@ -1,7 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Rocket {
-	private int speed;
-	private int distance;
-	private int totalAceleration;
-	private Propulsor p1, p2, p3;
-	private int gas;
+	private String name;
+	private double speed;
+	private double distance;
+	private double totalAceleration;
+	private List<Propulsor> propellers;
+	private double gas;
+	
+	public Rocket(String name, double gas) {
+		this.name = name;
+		this.gas = gas;
+		this.speed = 0;
+		this.distance = 0;
+		this.totalAceleration = 0;
+		this.propellers = new ArrayList<Propulsor>();
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public double getGas() {
+		return this.gas;
+	}
+	
+	public double getSpeed() {
+		return this.speed;
+	}
+	
+	public double getDistance() {
+		return this.distance;
+	}
+	
+	public double getTotalAceleration() {
+		return this.totalAceleration;
+	}
+	
+	public void addPropeller(Propulsor... propellers) {
+		for(Propulsor p : propellers) {
+			this.propellers.add(p);
+		}
+	}
 }
