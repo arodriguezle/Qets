7fe5fdea9899741d3d22010c82d671540f1593fe
Adrián <arodriguezle@edu.tecnocampus.cat>
2020-04-30 12:47:39 +0200

    track done

---
+++ Rockets/src/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+	public static void main(String[] args) {
+		Track track = new Track("FreeWorld", 1200.0, 18.0);
+
+	}
+}
---
+++ Rockets/src/Track.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Track {
+	private String name;
+	private double distance;
+	private double seconds;
+	private List<Rocket> rockets;
+
+	public Track(String name, double distance, double seconds) {
+		this.name = name;
+		this.distance = distance;
+		this.seconds = seconds;
+		this.rockets = new ArrayList<Rocket>();
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public double getDistance() {
+		return this.distance;
+	}
+
+	public double getSeconds() {
+		return this.seconds;
+	}
+
+	public void addRocket(Rocket rocket) {
+		this.rockets.add(rocket);
+	}
+
+}
