a2a3572621a776a4e19e33a6d7d1f3821f8ce830
Adrián <arodriguezle@edu.tecnocampus.cat>
2020-05-13 10:27:47 +0200

    66

--- Rockets/src/Main.java
+++ Rockets/src/Main.java
@@ -18,7 +18,6 @@ public class Main {
 			r.addPropeller(p1, p2);
 			track.addRocket(r);
 		}
-		// ENDS HERE
 		System.out.println("Select Algorithm:   (1=determinedSpeedAlgorithm   2=backtrackingAlgorithm)");
 		scan = new Scanner(System.in);
 		int selection = Integer.valueOf(scan.next());
