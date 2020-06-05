7ca441af9ef62210b9dd7fe441cb32f3e55f696f
Cristian Toison <ctoison@edu.tecnocampus.cat>
2020-06-05 11:40:10 +0200

    caqui

--- Rockets/src/CapaMain/Main.java
+++ Rockets/src/CapaMain/Main.java
@@ -4,25 +4,25 @@ import CapaAplication.*;
 import java.util.Scanner;
 
 public class Main {
-	private static int DETERMINEDSPEEDALGORIHTM = 1, BACKTRACKINGALGORITHM = 2;
+	private static int DETERMINED_SPEED_ALGORIHTM = 1, BACKTRACKING_ALGORITHM = 2;
 	private static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) throws Exception {
-		Rocket ViperX = new Rocket("Viper X");
+		Rocket rocket1 = new Rocket("Viper X");
 		GasTank gasTank = new GasTank(2500.0);
-		ViperX.addGasTank(gasTank);
+		rocket1.addGasTank(gasTank);
 		Propellant propeller1 = new Propellant(50);
 		Propellant propeller2 = new Propellant(20);
 		Propellant propeller3 = new Propellant(38);
-		ViperX.addPropellants(propeller1, propeller2, propeller3);
+		rocket1.addPropellants(propeller1, propeller2, propeller3);
 		Track track = new Track("FreeWorld", 1200.0, 18);
-		track.addRocket(ViperX);
+		track.addRocket(rocket1);
 		System.out.println(
 				"Select Algorithm:  \n1 => determinedSpeedAlgorithm   \n2 => backtrackingAlgorithm (Not finished yet, it is for the next delivery)");
 		int selection = Integer.valueOf(scan.next());
-		if (selection == DETERMINEDSPEEDALGORIHTM)
+		if (selection == DETERMINED_SPEED_ALGORIHTM)
 			track.startRace(track, 1);
-		else if (selection == BACKTRACKINGALGORITHM) {
+		else if (selection == BACKTRACKING_ALGORITHM) {
 			track.startRace(track, 2);
 		} else
 			throw new Exception("Not selected algorithm");
