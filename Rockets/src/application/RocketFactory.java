4f584fc6a9886d03ba591dc7251fea28a79e7d34
Cristian Toison <ctoison@edu.tecnocampus.cat>
2020-06-11 11:38:46 +0200

    xd

---
+++ Rockets/src/application/RocketFactory.java
@@ -0,0 +1,20 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import domain.*;
+
+public class RocketFactory {
+	
+	public RocketFactory() {
+		
+	}
+	
+	static List<Rocket> rockets = new ArrayList<Rocket>();
+	int propellantsViperX[] = {};
+	int propellantsFalconIX[] = {};
+	int propellantsStarV[] = {};
+	int propellantsSpeedyV[] = {};
+	
+}
