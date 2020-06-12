450e59c1eb129bc9002a23e1a792de2590f8e9d0
Cristian Toison <ctoison@edu.tecnocampus.cat>
2020-06-12 13:30:29 +0200

    vamos coñeeee

--- Rockets/src/utilities/RecordRepository.java
+++ Rockets/src/utilities/RecordRepository.java
@@ -1,7 +1,60 @@
 package utilities;
 
+import java.security.InvalidParameterException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.omg.CosNaming.NamingContextPackage.NotFound;
+
+import domain.Record;
+
 public class RecordRepository {
 	
-	public static void
+	public static void storeRecord(Record record) throws Exception {
+		try {
+			ConnectionBBDD connection = ConnectionRepository.getConnection();
+			
+			String sql = "Insert into REDCORDS (TRACK, ROCKET, TIME) VALUES (?, ?, ?)";
+			PreparedStatement pst = connection.prepareStatement(sql);
+			pst.setString(1, record.getTrackName());
+			pst.setString(2, record.getRocketName());
+			pst.setInt(3, record.getTime());
+			
+			if(pst.executeUpdate() != 1) {
+				throw new InvalidParameterException();
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+			throw new InvalidParamException();
+		}
+	}
+
+	public static Record getRecord(String idRecord) throws Exception {
+		ConnectionBBDD connection = ConnectionRepository.getConnection();
+		
+		try {
+			String sql = "SELECT * FROM RECORDS WHERE ID=?";
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.clearParameters();
+			preparedStatement.setString(1, idRecord);
+			ResultSet rs = preparedStatement.executeQuery();
+			
+			if(rs.next()) {
+				String id = rs.getString("TRACK");
+				int recordURL = rs.getInt("TIME");
+				String name = rs.getString("ROCKET");
+				return new Record(id, name, recordURL);
+			}
+			throw new NotFoundException();
+		}catch(SQLException e) {
+			e.printStackTrace();
+			throw new InvalidParamException();
+		}
+	}
+	
+	public static void updateRecord(Record record) {
+		
+	}
 	
 }
