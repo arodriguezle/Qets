e3e8b292b912930ec33d118d69c3b4d1db7dff89
Adrián <arodriguezle@edu.tecnocampus.cat>
2020-06-12 12:48:17 +0200

    push

--- Rockets/src/utilities/ConnectionRepository.java
+++ Rockets/src/utilities/ConnectionRepository.java
@@ -1,13 +1,12 @@
 package utilities;
 
 public class ConnectionRepository {
-	
+
 	private static ConnectionBBDD connection;
-	
-	static ConnectionBBDD getConnection() throws Exception{
-		if(connection == null) {
+
+	static ConnectionBBDD getConnection() throws Exception {
+		if (connection == null)
 			connection = new ConnectionBBDD(ConstantUtilities.USERNAME_BBDD, ConstantUtilities.PASSWORD_BBDD);
-		}
 		return connection;
 	}
 }
