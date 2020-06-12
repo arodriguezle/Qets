a7eacb996014105974ba4765b77da72826d996ea
Adrián <arodriguezle@edu.tecnocampus.cat>
2020-06-12 13:06:37 +0200

    record updated

--- Rockets/src/domain/Record.java
+++ Rockets/src/domain/Record.java
@@ -1,17 +1,29 @@
 package domain;
 
 public class Record {
-	String trackName;
-	String rocketName;
-	int time;
-	
+	private String trackName;
+	private String rocketName;
+	private int time;
+
 	public Record() {
-		
+
 	}
-	
-	public Record(String t, String r, int time) {
-		this.trackName = t;
-		this.rocketName = r;
+
+	public Record(String trackName, String rocketName, int time) {
+		this.trackName = trackName;
+		this.rocketName = rocketName;
 		this.time = time;
+	}
+
+	public int getTime() {
+		return this.time;
+	}
+
+	public String getRocketName() {
+		return this.rocketName;
+	}
+
+	public String getTrackName() {
+		return this.trackName;
 	}
 }
